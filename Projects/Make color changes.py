bf061801056396be97ca98ad75d16f12587d3b49
aliasghar <aliasghar.deeplearning.26@gmail.com>
2026-07-09 20:40:21 +0330

    Changing the color model

--- Projects/Make color changes.py
+++ Projects/Make color changes.py
@@ -7,6 +7,11 @@ cap = cv2.VideoCapture(0)
 while True:
 
     ret, frame = cap.read()
+
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
     cv2.imshow("Webcam", frame)
 
     if cv2.waitKey(1) == ord('q'):
